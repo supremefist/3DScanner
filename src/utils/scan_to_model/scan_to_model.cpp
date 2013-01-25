3ff6b04ce6f43bb03a6f9c1949d00b962a7ca4d1
supremefist <sakkie99@gmail.com>
2013-01-25 14:32:55 +0200

    Initial project stuff

---
+++ src/utils/scan_to_model/scan_to_model.cpp
@@ -0,0 +1,23 @@
+// A simple program that computes the square root of a number
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "3DScannerConfig.h"
+ 
+int main (int argc, char *argv[])
+{
+  if (argc < 2)
+    {
+    fprintf(stdout,"%s Version %d.%d\n",
+            argv[0],
+            SCANNER_VERSION_MAJOR,
+            SCANNER_VERSION_MINOR);
+    fprintf(stdout,"Usage: %s number\n",argv[0]);
+    return 1;
+    }
+  double inputValue = atof(argv[1]);
+  double outputValue = sqrt(inputValue);
+  fprintf(stdout,"The square root of %g is %g\n",
+          inputValue, outputValue);
+  return 0;
+}
