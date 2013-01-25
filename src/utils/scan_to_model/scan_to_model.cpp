f719201b51258b078c6ded63ecfc4ffd34e66871
JJBurchell <burchell.john@gmail.com>
2013-01-25 15:23:28 +0200

    Added PCL to project

--- src/utils/scan_to_model/scan_to_model.cpp
+++ src/utils/scan_to_model/scan_to_model.cpp
@@ -3,21 +3,27 @@
 #include <stdlib.h>
 #include <math.h>
 #include "3DScannerConfig.h"
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
  
 int main (int argc, char *argv[])
 {
-  if (argc < 2)
-    {
-    fprintf(stdout,"%s Version %d.%d\n",
-            argv[0],
-            SCANNER_VERSION_MAJOR,
-            SCANNER_VERSION_MINOR);
-    fprintf(stdout,"Usage: %s number\n",argv[0]);
-    return 1;
-    }
-  double inputValue = atof(argv[1]);
-  double outputValue = sqrt(inputValue);
-  fprintf(stdout,"The square root of %g is %g\n",
-          inputValue, outputValue);
+    pcl::PointCloud<pcl::PointXYZ> cloud;
+
+    // Fill in the cloud data
+	cloud.width  = 15;
+	cloud.height = 1;
+	cloud.points.resize (cloud.width * cloud.height);
+
+	// Generate the data
+	for (size_t i = 0; i < cloud.points.size (); ++i)
+	{
+	  cloud.points[i].x = 1024 * rand () / (RAND_MAX + 1.0f);
+	  cloud.points[i].y = 1024 * rand () / (RAND_MAX + 1.0f);
+	  cloud.points[i].z = 1.0;
+	}
+
+	pcl::io::savePCDFileASCII ("test_pcd.pcd", cloud);
+
   return 0;
 }
