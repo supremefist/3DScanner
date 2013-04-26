52a8c8acef18dc537ee8df9b2e8e80dbe9cad922
John Burchell <john@riaan-unity-vm.(none)>
2013-04-26 12:12:47 +0200

    Init frame obj added

---
+++ src/components/frame/frame.cpp
@@ -0,0 +1,12 @@
+#include "frame.h"
+
+Frame::Frame()
+{
+
+}
+
+Frame::Frame(CloudPtr cloud_in)
+{
+	_cloud = cloud_in;
+}
+
--- src/components/frame/frame.h
+++ src/components/frame/frame.h
@@ -1,4 +1,27 @@
 /*
-Class to describe for a frame
+Class Frame that holds all relevant info and data for a registration frame
 */
-#include <stdio.h>
+
+#ifndef FRAME_H
+#define FRAME_H
+
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+class Frame
+{
+	typedef pcl::PointCloud<pcl::PointXYZ> Cloud;
+	typedef Cloud::Ptr CloudPtr;
+
+public:
+	//members
+	CloudPtr _cloud;
+
+	//constructors
+	Frame();
+	Frame(CloudPtr);
+
+	//functions
+};
+
+#endif // FRAME_H
